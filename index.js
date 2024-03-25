258c263426353021887b20f013b0e75817ff0adc
Tommi <tommi.savolainen79@gmail.com>
2024-03-25 13:32:50 +0200

    port muutettu

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/api/persons', (request, response) => {
     morgan.token('data', (request) => JSON.stringify(request.body));
     response.json(person);
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
