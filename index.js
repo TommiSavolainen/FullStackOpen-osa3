b14b90e4b492b34fe9ee94be3e291678599f59a5
Tommi <tommi.savolainen79@gmail.com>
2024-03-27 12:38:05 +0200

    Poisto lisätty

--- index.js
+++ index.js
@@ -3,65 +3,44 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 app.use(express.json());
+app.use(requestLogger);
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 morgan.token('data', (request) => JSON.stringify(request.body));
 const cors = require('cors');
 app.use(cors());
 app.use(express.static('dist'));
 const Person = require('./models/person');
-// let persons = [
-//     {
-//         id: 1,
-//         name: 'Arto Hellas',
-//         number: '040-123456',
-//     },
-//     {
-//         id: 2,
-//         name: 'Ada Lovelace',
-//         number: '39-44-5323523',
-//     },
-//     {
-//         id: 3,
-//         name: 'Dan Abramov',
-//         number: '12-43-234345',
-//     },
-//     {
-//         id: 4,
-//         name: 'Mary Poppendick',
-//         number: '39-23-6423122',
-//     },
-// ];
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then((person) => {
         response.json(person);
     });
 });
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id);
-//     const person = persons.find((person) => person.id === id);
-//     if (person) {
-//         response.json(person);
-//     } else {
-//         response.status(404).end();
-//     }
-// });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
-
-    response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then((result) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
 });
 
-app.get('/info', (request, response) => {
-    const day = new Date();
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${day}</p>`);
-});
-
-// app.get('/api/persons', (request, response) => {
-//     response.json(persons);
+// app.get('/info', (request, response) => {
+//     const day = new Date();
+//     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${day}</p>`);
 // });
+
+app.get('/api/persons:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then((person) => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
+});
 
 // const generateId = () => {
 //     const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
@@ -89,12 +68,21 @@ app.post('/api/persons', (request, response) => {
     person.save().then((savedPerson) => {
         response.json(savedPerson);
     });
-    // persons = persons.concat(person);
 
-    // console.log(person);
     morgan.token('data', (request) => JSON.stringify(request.body));
-    // response.json(person);
 });
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    next(error);
+};
+
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
