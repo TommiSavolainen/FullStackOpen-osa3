4f662a8a0e7da66f683779a04a6e87a0082436ed
Tommi <tommi.savolainen79@gmail.com>
2024-03-28 09:15:49 +0200

    uppia

--- models/person.js
+++ models/person.js
@@ -16,7 +16,11 @@ mongoose
     });
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        minlenght: 3,
+        required: true,
+    },
     number: String,
 });
 
