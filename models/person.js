d8c72f69bfcd5325e77678a68f65f909f59f8016
Tommi <tommi.savolainen79@gmail.com>
2024-03-28 11:02:38 +0200

    joko nyt toimii

--- models/person.js
+++ models/person.js
@@ -26,7 +26,7 @@ const personSchema = new mongoose.Schema({
         // minlength: 8,
         validate: {
             validator: function (v) {
-                return /\d{2,3}-\d{7,8}/.test(v);
+                return /^\d{2,3}-\d{7,8}$/.test(v);
             },
             message: (props) => `${props.value} is not a valid phone number!`,
         },
